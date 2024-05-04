e2094e773aceb88ded795405826485d3897095b3
2457104-Simphiwe <2457104@students.wits.ac.za>
2024-05-04 10:18:37 +0200

    tests ran

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
-// some-module.test.js
-const sum = require('../src');
+// sum.test.js
+const sum = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -11,4 +11,4 @@ test('adds 0 + 0 to equal 0', () => {
 
 test('adds -1 + 1 to equal 0', () => {
   expect(sum(-1, 1)).toBe(0);
-});
+}); 
